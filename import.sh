1be10bfdbd8e7819820dd15fc9b4f9a7527d8cfa
Robin Wohlers-Reichel <me@robinwohlers.com>
2016-04-13 13:31:01 +1000

    import script
    Run the script in the directory you want to import into.
    Drag the source as the first parameter

---
+++ import.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -uo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+IMPORT=${1}
+
+SOURCES=$IMPORT/Sources/*
+
+for FILE in $SOURCES
+do
+    BASE=$(basename $FILE)
+    sed 's/\r$//' $FILE > $DIR/Sources/$BASE
+done
+
+#sed 's/\r$//' $IMPORT/Sources/Cmd.h > $DIR/Sources/Cmd.h
